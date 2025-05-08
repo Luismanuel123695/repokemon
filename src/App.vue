078c55a1e95c13fa81df6093fe506fd6e642bea3
Luis <luis.romo@cceo.com.mx>
2025-05-08 14:27:56 -0600

    feat: :sparkles: add images of the pokemons

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,34 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
+    <RouterLink class="navbar-brand" to="/">
+      <img 
+      src="@/assets/logo.png" 
+      alt="Logo" 
+      width="30" 
+      height="24" 
+      class="d-inline-block align-text-top">
+      
+    </RouterLink>Pokeapi
+  </div>
+  <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/Pokemons">pokemons</RouterLink>
 
       </nav>
+</nav>
+
+  <header>
+     
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+
     </div>
   </header>
 
--- src/views/PokeView.vue
+++ src/views/PokeView.vue
@@ -1,4 +1,37 @@
+<script setup>
+import axios from 'axios';
+import {useRoute ,useRouter} from 'vue-router';
+import {ref} from 'vue';
+const pokem = ref([]);
+const route = useRoute();
+const router = useRouter();
+
+
+
+const back = () => {
+    router.push('/pokemons')
+}
+
+
+const getData = async () => {
+    try {
+        const {data} = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
+        );
+        console.log(data);
+        pokem.value = data;
+    }catch (error) {
+        console.error(error);
+    }
+};
+getData();
+
+</script>
 
 <template>
-    <h1>Poke name:</h1>
+    
+  <img :src="pokem.sprites?.front_default" alt="">
+    <h1>Poke name: {{ $route.params.name }}</h1>
+    <button @click="back">Back</button>
+
 </template>
