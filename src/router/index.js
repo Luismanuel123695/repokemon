8443365407c3f47e28ee20a3935d1e03e70b6de8
Luis <luis.romo@cceo.com.mx>
2025-05-08 13:19:50 -0600

    feat: :sparkles: create route for pokeview

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,13 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/PokemonsView.vue'),
     },
+
+    {
+      path: "/pokemons/:name",
+      name: 'poke',
+      component: () => import('../views/PokeView.vue'),
+    },
+
   ],
 })
 
---
+++ src/views/PokeView.vue
@@ -0,0 +1,4 @@
+
+<template>
+    <h1>Poke name:</h1>
+</template>
