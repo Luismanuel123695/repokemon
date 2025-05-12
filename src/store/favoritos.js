f8e9d40d9a44c713ef33680393e174cd7a4a34bd
Luis <luis.romo@cceo.com.mx>
2025-05-12 12:30:16 -0600

    feat(favorites): :sparkles: persist favorites with localStorage

--- src/store/favoritos.js
+++ src/store/favoritos.js
@@ -1,11 +1,17 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
+
 export const useFavoritosStore = defineStore('favoritos', () => {
     const favoritos = ref([])
 
+
+    if(localStorage.getItem("favoritos")){
+        favoritos.value = JSON.parse(localStorage.getItem("favoritos"));
+    }
     const add = (poke) =>{
         favoritos.value.push(poke)
+        localStorage.setItem("favoritos", JSON.stringify(favoritos.value));
 
     };
     const remove = (id) => {
